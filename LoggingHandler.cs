576fe8026dad52134c91c192d8a825d48ef2dd0f
Sintaxya <newton.pwns@gmail.com>
2023-09-12 18:48:54 +0200

    LoggingHandler: Added. Will handle all logging related functions and features.

---
+++ LoggingHandler.cs
@@ -0,0 +1,23 @@
+﻿using Serilog;
+using Serilog.Core;
+using System;
+
+namespace ProtocolGateway
+{
+    class LoggingHandler
+    {
+        public static Logger? ConsoleLogger, FileLogger;
+
+        //Logger GetLoggerInstance()
+        //{
+        //    if (gwParam.logging_to_console)
+        //    {
+        //        ConsoleLogger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+        //    }
+        //    if (gwParam.logging_to_file)
+        //    {
+        //        FileLogger = new LoggerConfiguration().WriteTo.File($"gw_{gwId}").CreateLogger();
+        //    }
+        //}
+    }
+    }
