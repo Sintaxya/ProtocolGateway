fd7df54deda3623c51460f747b25dfde7ecc9690
fejes.gergo <fejes.gergo@infopolus.hu>
2023-09-15 11:47:51 +0200

    typo fix

---
+++ Protocols/ISharedNetworkComponents.cs
@@ -0,0 +1,11 @@
+﻿namespace ProtocolGateway.Protocols
+{
+    public interface ISharedNetworkComponents
+    {
+        bool Status { get; }
+        void Receive();
+        void Send();
+        LoggingHandler LoggingHandler { get; set; }
+        ProtocolGatewayHandler GatewayHandler { get; set; }
+    }
+}
